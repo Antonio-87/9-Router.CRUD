8b5548561331df2cff56e18d9307f06599f33615
Antonio-87 <davydenko.87@list.ru>
2024-01-22 11:25:46 +0300

    PostCreate_iconClose

--- router-crud/src/App.tsx
+++ router-crud/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PostsList from "./components/PostsList";
 import PostView from "./components/PostView";
+import PostCreate from "./components/PostCreate";
 
 function App() {
   return (
---
+++ router-crud/src/components/PostCreate.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import useJsonFetch from "../hooks/useJsonFetch";
+import { useNavigate } from "react-router-dom";
+import iconClose from "../assets/close.svg";
+
+const PostCreate = () => {
+  const [value, setValue] = useState<string>("");
+  const navigate = useNavigate();
+
+  const addPost = () => {
+    useJsonFetch(`${import.meta.env.VITE_POSTS_URL}posts`, {
+      body: JSON.stringify({ id: 0, content: value }),
+    });
+  };
+
+  return (
+    <div className="window-create">
+      <header>
+        <img
+          className="close"
+          src={iconClose}
+          alt="close"
+          onClick={() => navigate(-1)}
+        />
+      </header>
+      <main>
+        <textarea
+          className="input-content"
+          cols={30}
+          rows={10}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onClick={() => {
+            addPost();
+            navigate(-1);
+          }}
+        ></textarea>
+      </main>
+      <footer>
+        <button className="create">Опубликовать</button>
+      </footer>
+    </div>
+  );
+};
+
+export default PostCreate;
